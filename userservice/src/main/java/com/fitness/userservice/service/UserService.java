efdcdeacb54c7bc61934b0bd936d1d78a1ba9af7
RG ARAVINDHA KRUSHNAN <rgak2@rgak.com>
2025-10-10 01:36:20 +0530

    feat(eureka): completed Eureka integration and interservice communication
    - Added Eureka Server configuration
    - Registered UserService and ActivityService as Eureka clients
    - Verified successful communication between services via service discovery

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -5,10 +5,12 @@ import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.model.User;
 import com.fitness.userservice.repository.UserRepository;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -53,5 +55,10 @@ public class UserService {
         userResponse.setUpdatedAt(user.getUpdatedAt());
 
         return userResponse;
+    }
+
+    public Boolean existByUserId(String userId) {
+        log.info("Calling User Validation API for userID: " + userId);
+        return repository.existsById(userId);
     }
 }
